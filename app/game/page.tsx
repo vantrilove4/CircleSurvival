ea80ed0f9e45579262037f35cd9a4f2497bd7fd9
vantrilove4 <nvantri93@gmail.com>
2025-12-25 15:57:40 +0700

    Move game files to public/game

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,7 +2,7 @@ export default function GamePage() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <iframe
-        src="/CircleSurvival.html"
+        src="/game/CircleSurvival.html"
         style={{ width: '100%', height: '100%', border: 'none' }}
       ></iframe>
     </div>
