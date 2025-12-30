df8943f56c404b9275db7cb1c648864fa3e466b1
vantrilove4 <nvantri93@gmail.com>
2025-12-30 13:17:23 +0700

    Update analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 //import './globals.css'
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: 'CircleSurvival 3D',
   description: 'Play CircleSurvival 3D - Endless survival game in browser!',
